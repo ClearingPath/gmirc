c846ba65d9279c6454bd754cb45f573e52b00fbc
Felicia <feli-c-s@hotmail.com>
2015-09-27 05:07:00 +0900

    Iseng implementasi ngide

---
+++ gMIRC_maven_server/src/main/java/gMIRC_handler.java
@@ -0,0 +1,439 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
+import gMIRC.ResCode;
+import gMIRC.UpdateString;
+import gMIRC.grpcMIRCGrpc;
+import java.net.UnknownHostException;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
+
+import gMIRC.Username;
+import gMIRC.UserChannel;
+import gMIRC.UserMessage;
+import io.grpc.stub.StreamObserver;
+
+/**
+ *
+ * @author Jonathan
+ */
+public class gMIRC_handler implements grpcMIRCGrpc.grpcMIRC {
+
+  @Override
+  public void join(gMIRC.UserChannel request,
+	  io.grpc.stub.StreamObserver<gMIRC.ResCode> responseObserver) {
+    int responseCode = AddChannel(request.getUsername(), request.getChannelname());
+    ResCode reply = ResCode.newBuilder().setResponseCode(responseCode).build();
+    responseObserver.onValue(reply);
+    responseObserver.onCompleted();
+  }
+
+  @Override
+  public void regUser(Username request, StreamObserver<ResCode> responseObserver) {
+    //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    int responseCode = AddUser(request.getUsername());
+    ResCode reply = ResCode.newBuilder().setResponseCode(responseCode).build();
+    responseObserver.onValue(reply);
+    responseObserver.onCompleted();
+  }
+
+  @Override
+  public void leave(UserChannel request, StreamObserver<ResCode> responseObserver) {
+    //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    
+    int responseCode = DeleteChannelUser(request.getChannelname(), request.getUsername());
+    ResCode reply = ResCode.newBuilder().setResponseCode(responseCode).build();
+    responseObserver.onValue(reply);
+    responseObserver.onCompleted();
+  }
+
+  @Override
+  public void exit(Username request, StreamObserver<ResCode> responseObserver) {
+    //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    System.out.println(request.getUsername() + " exiting...");
+//        return SoftDelete(username);
+    ResCode reply = ResCode.newBuilder().setResponseCode(0).build();
+    responseObserver.onValue(reply);
+    responseObserver.onCompleted();
+  }
+
+  @Override
+  public void message(UserMessage request, StreamObserver<ResCode> responseObserver) {
+    //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    int ret = 0;
+    if (request.getChannelname().equalsIgnoreCase("*")) {
+      ret = PutMessageWild(request.getUsername(), request.getMsg());
+    } else {
+      ret = PutMessage(request.getUsername(), request.getChannelname(), request.getMsg());
+    }
+    ResCode reply = ResCode.newBuilder().setResponseCode(ret).build();
+    responseObserver.onValue(reply);
+    responseObserver.onCompleted();
+    
+  }
+
+  @Override
+  public void regularUpdates(Username request, StreamObserver<UpdateString> responseObserver) {
+    //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    String ret = GetMessage(request.getUsername());
+    UpdateString reply = UpdateString.newBuilder().setNewMessages(ret).build();
+    responseObserver.onValue(reply);
+    responseObserver.onCompleted();
+    
+  }
+
+
+
+  /**
+   * Add a user to a channel (can check if the channel is a new channel)
+   *
+   * @param Username
+   * @param Channel
+   * @return code
+   */
+  private int AddChannel(String Username, String Channel) {
+    int ret = 0;
+    try {
+
+      MongoClient mongoClient = new MongoClient();
+      DB db = mongoClient.getDB("mirc");
+      DBCollection coll = db.getCollection("channelCollection");
+      BasicDBObject query = new BasicDBObject("username", Username)
+	      .append("channel", Channel);
+
+      DBCursor cursor = coll.find(query);
+
+      try {
+	if (cursor.hasNext()) {
+	  ret = 1;
+	  System.err.println(Username + " has joined Channel : " + Channel + "!");
+	} else {
+	  query = new BasicDBObject("channel", Channel);
+	  DBCursor cursor2 = coll.find(query);
+	  try {
+	    if (!cursor2.hasNext()) {
+	      ret = 2;
+	    }
+	  } finally {
+	    cursor2.close();
+	  }
+	  BasicDBObject doc = new BasicDBObject("username", Username)
+		  .append("channel", Channel);
+	  coll.insert(doc);
+	  System.out.println(Username + " joined Channel : " + Channel);
+	}
+      } finally {
+	cursor.close();
+      }
+
+    } catch (UnknownHostException ex) {
+      Logger.getLogger(gMIRC_handler.class.getName()).log(Level.SEVERE, null, ex);
+    }
+    return ret;
+  }
+
+  /**
+   * Delete a user from a channel (used in leave method)
+   *
+   * @param Channel
+   * @param Username
+   * @return code
+   */
+  private int DeleteChannelUser(String Channel, String Username) {
+    int ret = 0;
+    try {
+
+      MongoClient mongoClient = new MongoClient();
+      DB db = mongoClient.getDB("mirc");
+      DBCollection coll = db.getCollection("channelCollection");
+      BasicDBObject query = new BasicDBObject("username", Username)
+	      .append("channel", Channel);
+
+      DBCursor cursor = coll.find(query);
+
+      try {
+	while (cursor.hasNext()) {
+	  coll.remove(cursor.next());
+	}
+      } finally {
+	cursor.close();
+      }
+
+    } catch (UnknownHostException ex) {
+      Logger.getLogger(gMIRC_handler.class.getName()).log(Level.SEVERE, null, ex);
+    }
+    System.out.println(Username + " has leaved Channel : " + Channel);
+    return ret;
+  }
+
+  /**
+   * Delete a user in all channel (used in cleaning)
+   *
+   * @param Username
+   * @return code
+   */
+  public int DeleteUserInChannel(String Username) {
+    int ret = 0;
+    try {
+
+      MongoClient mongoClient = new MongoClient();
+      DB db = mongoClient.getDB("mirc");
+      DBCollection coll = db.getCollection("channelCollection");
+      BasicDBObject query = new BasicDBObject("username", Username);
+
+      DBCursor cursor = coll.find(query);
+
+      try {
+	while (cursor.hasNext()) {
+	  coll.remove(cursor.next());
+	}
+      } finally {
+	cursor.close();
+      }
+
+    } catch (UnknownHostException ex) {
+      Logger.getLogger(gMIRC_handler.class.getName()).log(Level.SEVERE, null, ex);
+    }
+    System.out.println(Username + " has been deleted in Channel Collection!");
+    return ret;
+  }
+
+  /**
+   * Register a user to the server (used in RegUser)
+   *
+   * @param username
+   * @return code
+   */
+  private int AddUser(String username) {
+    int ret = 0;
+    try {
+
+      MongoClient mongoClient = new MongoClient();
+      DB db = mongoClient.getDB("mirc");
+      DBCollection coll = db.getCollection("activeUser");
+      BasicDBObject query = new BasicDBObject("username", username);
+
+      DBCursor cursor = coll.find(query);
+
+      try {
+	if (cursor.hasNext()) {
+	  Date now = new Date();
+	  long timestamp_now = now.getTime();
+	  long treshold = timestamp_now - (1000 * 10); //10
+	  BasicDBObject temp = (BasicDBObject) cursor.next();
+	  Date time_temp = (Date) temp.get("timestamp");
+	  long timestamp_temp = time_temp.getTime();
+	  if (timestamp_temp < treshold) {
+	    ret = 2;
+	    System.out.println(username + " has joined back!");
+	  } else {
+	    ret = 1;
+	    System.err.println(username + " has been used !");
+	  }
+	} else {
+	  java.util.Date date = new java.util.Date();
+	  BasicDBObject doc = new BasicDBObject("username", username)
+		  .append("timestamp", date);
+	  coll.insert(doc);
+	  System.out.println(username + " online !");
+	}
+      } finally {
+	cursor.close();
+      }
+
+    } catch (UnknownHostException ex) {
+      Logger.getLogger(gMIRC_handler.class.getName()).log(Level.SEVERE, null, ex);
+    }
+    return ret;
+  }
+
+  private int UpdateLastActive(String username) {
+    int ret = 0;
+    try {
+
+      MongoClient mongoClient = new MongoClient();
+      DB db = mongoClient.getDB("mirc");
+      DBCollection coll = db.getCollection("activeUser");
+      BasicDBObject query = new BasicDBObject("username", username);
+
+      DBCursor cursor = coll.find(query);
+
+      try {
+	if (cursor.hasNext()) {
+	  DBObject temp = cursor.next();
+	  java.util.Date date = new java.util.Date();
+	  temp.put("timestamp", date);
+	  coll.save(temp);
+	} else {
+	  java.util.Date date = new java.util.Date();
+	  BasicDBObject doc = new BasicDBObject("username", username)
+		  .append("timestamp", date);
+	  coll.insert(doc);
+	  System.out.println(username + " online !");
+	}
+      } finally {
+	cursor.close();
+      }
+
+    } catch (UnknownHostException ex) {
+      Logger.getLogger(gMIRC_handler.class.getName()).log(Level.SEVERE, null, ex);
+    }
+    return 0;
+  }
+
+  /**
+   * Moved active user to a passive user (soon to be deleted)
+   *
+   * @param username
+   * @return code
+   */
+  public int SoftDelete(String username) {
+    int ret = 0;
+    try {
+      MongoClient mongoClient = new MongoClient();
+      DB db = mongoClient.getDB("mirc");
+      DBCollection coll = db.getCollection("activeUser");
+      DBCollection coll2 = db.getCollection("passiveUser");
+      BasicDBObject query = new BasicDBObject("username", username);
+
+      DBCursor cursor = coll.find(query);
+
+      try {
+	if (cursor.hasNext()) {
+	  DBObject temp = cursor.next();
+	  coll2.insert(temp);
+	  coll.remove(temp);
+	} else {
+	  ret = 1;
+	}
+      } finally {
+	cursor.close();
+      }
+    } catch (UnknownHostException ex) {
+      Logger.getLogger(gMIRC_handler.class.getName()).log(Level.SEVERE, null, ex);
+    }
+    return ret;
+  }
+
+  public String GetMessage(String username) {
+    String ret = "";
+    try {
+      MongoClient mongoClient = new MongoClient();
+      DB db = mongoClient.getDB("mirc");
+      DBCollection coll = db.getCollection("inbox");
+      BasicDBObject query = new BasicDBObject("target", username);
+      JSONObject obj = new JSONObject();
+      JSONArray arr = new JSONArray();
+      DBCursor cursor = coll.find(query);
+
+      try {
+	while (cursor.hasNext()) {
+	  BasicDBObject temp = (BasicDBObject) cursor.next();
+	  JSONObject sav = new JSONObject();
+	  sav.put("target", temp.getString("target"));
+	  sav.put("username", temp.getString("username"));
+	  sav.put("channel", temp.getString("channel"));
+	  sav.put("message", temp.getString("message"));
+	  sav.put("timestamp", temp.getLong("timestamp"));
+	  arr.add(sav);
+	  coll.remove(temp);
+	}
+	obj.put("msg", arr);
+	ret = obj.toJSONString();
+      } finally {
+	cursor.close();
+      }
+    } catch (UnknownHostException ex) {
+      Logger.getLogger(gMIRC_handler.class.getName()).log(Level.SEVERE, null, ex);
+    }
+    UpdateLastActive(username);
+    return ret;
+  }
+
+  private int PutMessage(String username, String channelname, String msg) {
+    int ret = 0;
+    try {
+      MongoClient mongoClient = new MongoClient();
+      DB db = mongoClient.getDB("mirc");
+      DBCollection coll = db.getCollection("inbox");
+      DBCollection coll2 = db.getCollection("channelCollection");
+      BasicDBObject query2 = new BasicDBObject("channel", channelname);
+      BasicDBObject query = new BasicDBObject("channel", channelname)
+	      .append("username", username);
+      DBCursor cursor = coll2.find(query);
+      try {
+	if (cursor.hasNext()) {
+	  DBCursor cursor2 = coll2.find(query2);
+	  System.out.println("Got message from " + username);
+	  try {
+	    java.util.Date date = new java.util.Date();
+	    while (cursor2.hasNext()) {
+	      ret = 1;
+	      BasicDBObject temp = (BasicDBObject) cursor2.next();
+	      String target = temp.get("username").toString();
+	      BasicDBObject put = new BasicDBObject("target", target)
+		      .append("username", username)
+		      .append("channel", channelname)
+		      .append("message", msg)
+		      .append("timestamp", date.getTime());
+	      coll.insert(put);
+	      ret = 0;
+	    }
+	  } finally {
+	    cursor2.close();
+	  }
+	} else {
+	  ret = 2;
+	  System.out.println(username + " not registered to Channel : " + channelname);
+	}
+      } finally {
+	cursor.close();
+      }
+
+    } catch (UnknownHostException ex) {
+      ret = 1;
+      Logger.getLogger(gMIRC_handler.class.getName()).log(Level.SEVERE, null, ex);
+    }
+    return ret;
+  }
+
+  private int PutMessageWild(String username, String msg) {
+    int ret = 0;
+
+    try {
+      MongoClient mongoClient = new MongoClient();
+      DB db = mongoClient.getDB("mirc");
+      DBCollection coll2 = db.getCollection("channelCollection");
+      BasicDBObject query = new BasicDBObject("username", username);
+      System.out.println("Wild message appear from " + username + " !");
+      DBCursor cursor = coll2.find(query);
+      try {
+	while (cursor.hasNext()) {
+	  ret = 1;
+	  BasicDBObject temp = (BasicDBObject) cursor.next();
+	  String channelname = temp.getString("channel");
+	  ret = PutMessage(username, channelname, msg);
+	}
+      } finally {
+	cursor.close();
+      }
+    } catch (UnknownHostException ex) {
+      Logger.getLogger(gMIRC_handler.class.getName()).log(Level.SEVERE, null, ex);
+    }
+
+    return 0;
+  }
+
+}
---
+++ gMIRC_maven_server/src/main/java/gMIRC_server.java
@@ -0,0 +1,221 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
+import gMIRC.grpcMIRCGrpc;
+import gMIRC.grpcMIRCGrpc.grpcMIRC;
+import io.grpc.ServerImpl;
+import io.grpc.netty.NettyServerBuilder;
+import java.net.UnknownHostException;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Jonathan
+ */
+public class gMIRC_server {
+
+  private static final int port = 2121;
+  private static ServerImpl server;
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    try {
+
+      Runnable simple = new Runnable() {
+	public void run() {
+	  simple();
+	}
+      };
+
+      new Thread(simple).start();
+
+      Runnable cleaner = new Runnable() {
+	public void run() {
+	  try {
+	    Thread.sleep(100);
+	    while (true) {
+	      System.out.println("cleaner running...");
+	      cleaner();
+	      Thread.sleep(1000 * 60 * 5);
+	    }
+	  } catch (InterruptedException ex) {
+	    Logger.getLogger(gMIRC_server.class.getName()).log(Level.SEVERE, null, ex);
+	  }
+	}
+      };
+
+      new Thread(cleaner).start();
+
+      Runnable ultClean = new Runnable() {
+	public void run() {
+	  try {
+	    Thread.sleep(500);
+	    while (true) {
+	      UltimateClean();
+	      Thread.sleep(1000 * 60 * 60);
+	    }
+	  } catch (InterruptedException ex) {
+	    Logger.getLogger(gMIRC_server.class.getName()).log(Level.SEVERE, null, ex);
+	  }
+	}
+      };
+
+      new Thread(ultClean).start();
+    } catch (Exception x) {
+      x.printStackTrace();
+    }
+  }
+
+  public static void simple() {
+    try {
+
+      System.out.println("Starting simple server...");
+
+      server = NettyServerBuilder.forPort(port)
+	      .addService(grpcMIRCGrpc.bindService(new gMIRC_handler()))
+	      .build().start();
+      
+      Runtime.getRuntime().addShutdownHook(new Thread() {
+	@Override
+	public void run() {
+	  // Use stderr here since the logger may have been reset by its JVM shutdown hook.
+	  System.err.println("*** shutting down gRPC server since JVM is shutting down");
+	  gMIRC_server.stop();
+	  System.err.println("*** server shut down");
+	}
+      });
+      
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static void cleaner() {
+    try {
+      boolean hard_clean = false;
+      MongoClient mongoClient = new MongoClient();
+      DB db = mongoClient.getDB("mirc");
+      DBCollection coll = db.getCollection("activeUser");
+
+      DBCursor cursor = coll.find();
+      try {
+	Date now = new Date();
+	long timestamp_now = now.getTime();
+	long treshold = timestamp_now - (1000 * 60 * 5); //5 minutes
+	while (cursor.hasNext()) {
+	  hard_clean = true;
+	  BasicDBObject temp = (BasicDBObject) cursor.next();
+	  Date time_temp = (Date) temp.get("timestamp");
+	  long timestamp_temp = time_temp.getTime();
+	  if (timestamp_temp < treshold) {
+	    String target = temp.getString("username");
+	    //handler.SoftDelete(target);
+	  }
+	}
+	HardClean();
+      } finally {
+	cursor.close();
+      }
+
+    } catch (UnknownHostException ex) {
+      Logger.getLogger(gMIRC_server.class.getName()).log(Level.SEVERE, null, ex);
+    }
+  }
+
+  public static void HardClean() {
+    try {
+      MongoClient mongoClient = new MongoClient();
+      DB db = mongoClient.getDB("mirc");
+      DBCollection coll[] = new DBCollection[4];
+      coll[0] = db.getCollection("channelCollection");
+      coll[1] = db.getCollection("inbox");
+      coll[2] = db.getCollection("activeUser");
+      coll[3] = db.getCollection("passiveUser");
+
+      DBCursor cursor = coll[3].find();
+
+      try {
+	while (cursor.hasNext()) {
+	  BasicDBObject temp = (BasicDBObject) cursor.next();
+	  String username = temp.getString("username");
+	  BasicDBObject query = new BasicDBObject("username", username);
+	  System.out.println("cleaning " + username);
+	  for (int i = 0; i < 4; i++) {
+	    DBCursor cursor2 = coll[i].find(query);
+
+	    try {
+	      while (cursor2.hasNext()) {
+		DBObject temp2 = cursor2.next();
+		coll[i].remove(temp2);
+	      }
+	    } finally {
+	      cursor2.close();
+	    }
+	  }
+	}
+      } finally {
+	cursor.close();
+      }
+
+    } catch (UnknownHostException ex) {
+      Logger.getLogger(gMIRC_server.class.getName()).log(Level.SEVERE, null, ex);
+    }
+  }
+
+  /**
+   * ONLY IF NO USER IS IN THE ACTIVE OR PASIVE COLLECTION;getting server to
+   * clean state
+   */
+  private static void UltimateClean() {
+    try {
+      MongoClient mongoClient = new MongoClient();
+      DB db = mongoClient.getDB("mirc");
+      DBCollection coll[] = new DBCollection[4];
+      coll[0] = db.getCollection("activeUser");
+      coll[1] = db.getCollection("passiveUser");
+      coll[2] = db.getCollection("channelCollection");
+      coll[3] = db.getCollection("inbox");
+
+      DBCursor cursor[] = new DBCursor[4];
+      cursor[0] = coll[0].find();
+      cursor[1] = coll[1].find();
+      cursor[2] = coll[2].find();
+      cursor[3] = coll[3].find();
+      try {
+	if (!cursor[0].hasNext() && !cursor[1].hasNext() && cursor[2].hasNext() && cursor[3].hasNext()) {
+	  System.out.println("SYSTEM RESTARTING with ULTIMATE CLEANING !");
+	  for (int i = 2; i <= 3; i++) {
+	    coll[i].drop();
+	  }
+	  System.out.println("RESTART COMPLETE!");
+	}
+      } finally {
+	cursor[0].close();
+	cursor[1].close();
+      }
+    } catch (UnknownHostException ex) {
+      Logger.getLogger(gMIRC_server.class.getName()).log(Level.SEVERE, null, ex);
+    }
+  }
+
+  private static void stop() {
+    //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    
+    if (server != null) {
+      server.shutdown();
+    }
+  }
+}
